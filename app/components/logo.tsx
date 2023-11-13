fe64f015f233ca3e852fcc6802e7d33b914fc4a4
Dante Mai <mv.thienphu@gmail.com>
2023-11-12 21:50:48 -0500

    initial upload with basic layout

---
+++ app/components/logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Group , Title} from '@mantine/core'
+const Logo = () => {
+  return (
+    <Group align='center' justify='center' gap={0}>
+      <Title order={4} size={'h3'}>Tin Lành. Knoxville</Title>
+    </Group>
+  );
+}
+
+export default Logo
+
